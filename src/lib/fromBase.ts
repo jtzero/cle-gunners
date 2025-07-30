6c5fea1c56f2103197e3a254e09de2622e5d2b3c
Kyle Behrens <jtzero511@gmail.com>
2025-07-30 11:35:42 -0400

    Update fromBase to ignore external links

--- src/lib/fromBase.ts
+++ src/lib/fromBase.ts
@@ -2,7 +2,10 @@ export const fromBase = (path: string | undefined): string => {
   if (!path) {
     return import.meta.env.BASE_URL;
   }
-  if (path.startsWith("/") && import.meta.env.BASE_URL === "/") {
+  if (path.startsWith("http")) {
+    return path;
+  }
+  if (path.startsWith("/") && import.meta.env.BASE_URL === "/")  {
     return path;
   }
   return [import.meta.env.BASE_URL, path].filter(Boolean).join("/");
