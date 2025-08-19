58f69675bb52e8ec42b906bab02ada3bf7162c18
jtzero ? <jtzero511@gmail.com>
2025-08-19 12:41:17 -0400

    fix data-pipeline calling issues

--- bin/get-data.ts
+++ bin/get-data.ts
@@ -1,9 +1,6 @@
-
-import * as fs from 'fs';
+import * as fs from "fs";
 import { format, startOfWeek, endOfWeek } from "date-fns";
-import * as appRoot from 'app-root-path';
-
-const api_key = process.env.API_SPORTS_API_KEY!;
+import * as appRoot from "app-root-path";
 
 const fetchTeamID = async (api_key: string) => {
   const headers = new Headers();
@@ -14,20 +11,24 @@ const fetchTeamID = async (api_key: string) => {
     headers: headers,
     redirect: "follow",
   };
-  const API_ENDPOINT = `https://v3.football.api-sports.io/teams?name=arsenal`;
-  try {
-    const response = await fetch(API_ENDPOINT, requestOptions);
+  const api_endpoint = `https://v3.football.api-sports.io/teams?name=arsenal`;
+  const response = await fetch(api_endpoint, requestOptions);
 
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-
-    const data = await response.json();
-    return data["team"]["id"];
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
   }
+
+  const data = await response.json();
+  console.log(data);
+  return data["team"]["id"];
 };
 
-const fetchArsenalFixtures = async (api_key: string, id: string, startOfThisWeek: string, endOfThisWeek: string) => {
+const fetchArsenalFixtures = async (
+  api_key: string,
+  id: string,
+  startOfThisWeek: string,
+  endOfThisWeek: string,
+) => {
   const headers = new Headers();
   headers.append("x-apisports-key", api_key);
 
@@ -37,24 +38,19 @@ const fetchArsenalFixtures = async (api_key: string, id: string, startOfThisWeek
     redirect: "follow",
   };
 
-  const API_ENDPOINT = `https://v3.football.api-sports.io/fixtures?team=${TEAM_ID}&from=${startOfThisWeek}&to=${endOfThisWeek}`;
-  try {
-    const response = await fetch(API_ENDPOINT, requestOptions);
+  const API_ENDPOINT = `https://v3.football.api-sports.io/fixtures?team=${id}&from=${startOfThisWeek}&to=${endOfThisWeek}`;
+  const response = await fetch(API_ENDPOINT, requestOptions);
 
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-
-    const data = await response.json();
-
-    return data.results
-  } catch (error) {
-    console.error("Error fetching data:", error);
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
   }
+
+  const data = await response.json();
+
+  return data.results;
 };
 
 const writeDataToFile = async (filePath: string, data: any) => {
-
   try {
     // Convert the JavaScript object to a JSON string
     // The 'null' and '2' arguments format the JSON with an indentation of 2 spaces, making it readable
@@ -69,19 +65,34 @@ const writeDataToFile = async (filePath: string, data: any) => {
   }
 };
 
-const id = await fetchTeamID(api_key);
-//https://media.api-sports.io/football/teams/{team_id}.png
-// Get the start and end of the current week (Sunday to Saturday)
-const today = new Date();
-const startOfThisWeek = format(
-  startOfWeek(today, { weekStartsOn: 1 }),
-  "yyyy-MM-dd",
-); // Monday
-const endOfThisWeek = format(
-  endOfWeek(today, { weekStartsOn: 1 }),
-  "yyyy-MM-dd",
-); // Sunday
+try {
+  console.log("Fetching data from API Sports...");
+  const api_key = process.env.API_SPORTS_API_KEY!;
+  if (!api_key) {
+    throw new Error("API key not found in environment variables.");
+  }
 
-const results = fetchArsenalFixtures(api_key, id, startOfThisWeek, endOfThisWeek);
-const filePath = `${appRoot.path}/src/fixtures/{startOfThisWeek}-{endOfThisWeek}.json`;
-writeDataToFile(filePath, results);
+  const id = await fetchTeamID(api_key);
+  //https://media.api-sports.io/football/teams/{team_id}.png
+  // Get the start and end of the current week (Sunday to Saturday)
+  const today = new Date();
+  const startOfThisWeek = format(
+    startOfWeek(today, { weekStartsOn: 1 }),
+    "yyyy-MM-dd",
+  ); // Monday
+  const endOfThisWeek = format(
+    endOfWeek(today, { weekStartsOn: 1 }),
+    "yyyy-MM-dd",
+  ); // Sunday
+
+  const results = fetchArsenalFixtures(
+    api_key,
+    id,
+    startOfThisWeek,
+    endOfThisWeek,
+  );
+  const filePath = `${appRoot.path}/src/fixtures/{startOfThisWeek}-{endOfThisWeek}.json`;
+  writeDataToFile(filePath, results);
+} catch (error) {
+  console.error("Error fetching data:", error);
+}
