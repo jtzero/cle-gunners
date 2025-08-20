9c69766b4dd565ffea4fd84bcc7d1548389e5c53
jtzero ? <jtzero511@gmail.com>
2025-08-20 09:28:09 -0400

    switches to FOOTBALL_DATA_API_KEY

--- bin/get-data.ts
+++ bin/get-data.ts
@@ -2,34 +2,59 @@ import * as fs from "fs";
 import { format, startOfWeek, endOfWeek } from "date-fns";
 import appRoot from "app-root-path";
 
-const fetchTeamID = async (api_key: string) => {
+const fetchTeamID = async (api_key: string, season: number) => {
   const headers = new Headers();
-  headers.append("x-apisports-key", api_key);
+  headers.append("X-Auth-Token", api_key);
 
   const requestOptions: RequestInit = {
     method: "GET",
     headers: headers,
     redirect: "follow",
   };
-  const api_endpoint = `https://v3.football.api-sports.io/teams?name=arsenal&country=england`;
+  const api_endpoint = `https://api.football-data.org/v4/competitions/PL/teams?season=${season}`;
   const response = await fetch(api_endpoint, requestOptions);
 
+  const data = await response.json();
   if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
+    throw new Error(`HTTP error! resonse: ${data.Message}`);
   }
 
-  const data = await response.json();
-  return data.response[0]["team"]["id"];
+  return data.teams.filter((team: any) => team.shortName === "Arsenal")[0].id;
 };
 
-const fetchArsenalFixtures = async (
-  api_key: string,
+const fetchPremierLeageID = async (api_key: string) => {
+  const headers = new Headers();
+  headers.append("X-Auth-Token", api_key);
+
+  const requestOptions: RequestInit = {
+    method: "GET",
+    headers: headers,
+    redirect: "follow",
+  };
+  const api_endpoint = `https://api.football-data.org/v4/competitions/PL`;
+  const response = await fetch(api_endpoint, requestOptions);
+
+  const data = await response.json();
+  if (!response.ok) {
+    throw new Error(`HTTP error! resonse: ${data.Message}`);
+  }
+
+  return data.id;
+};
+
+const buildRequestURL = (
   id: string,
+  leagueID: string,
+  season: number,
   startOfThisWeek: string,
   endOfThisWeek: string,
 ) => {
+  return `https://api.football-data.org/v4/teams/${id}/matches?season=${season}&competitions=${leagueID}&dateFrom=${startOfThisWeek}&dateTo=${endOfThisWeek}`;
+};
+
+const fetchArsenalFixtures = async (api_key: string, requestURL: string) => {
   const headers = new Headers();
-  headers.append("x-apisports-key", api_key);
+  headers.append("X-Auth-Token", api_key);
 
   const requestOptions: RequestInit = {
     method: "GET",
@@ -37,7 +62,7 @@ const fetchArsenalFixtures = async (
     redirect: "follow",
   };
 
-  const api_endpoint = `https://v3.football.api-sports.io/fixtures?team=${id}&from=${startOfThisWeek}&to=${endOfThisWeek}`;
+  const api_endpoint = requestURL;
   const response = await fetch(api_endpoint, requestOptions);
 
   if (!response.ok) {
@@ -46,7 +71,7 @@ const fetchArsenalFixtures = async (
 
   const data = await response.json();
 
-  return data.results;
+  return data;
 };
 
 const writeDataToFile = async (filePath: string, data: any) => {
@@ -60,15 +85,13 @@ const writeDataToFile = async (filePath: string, data: any) => {
 
 try {
   console.log("Fetching data from API Sports...");
-  const api_key = process.env.API_SPORTS_API_KEY!;
+  const api_key = process.env.FOOTBALL_DATA_API_KEY!;
   if (!api_key) {
-    throw new Error("API key not found in environment variables.");
+    throw new Error(
+      "API key 'FOOTBALL_DATA_API_KEY' not found in environment variables.",
+    );
   }
 
-  console.log("Fetching team ID...");
-  const id = await fetchTeamID(api_key);
-  //https://media.api-sports.io/football/teams/{team_id}.png
-  // Get the start and end of the current week (Sunday to Saturday)
   const today = new Date();
   const startOfThisWeek = format(
     startOfWeek(today, { weekStartsOn: 1 }),
@@ -78,14 +101,22 @@ try {
     endOfWeek(today, { weekStartsOn: 1 }),
     "yyyy-MM-dd",
   ); // Sunday
+  console.log("Fetching League ID...");
+  const leagueID = await fetchPremierLeageID(api_key);
+  console.log("Fetching team ID...");
+  const id = await fetchTeamID(api_key, today.getFullYear());
+  console.log("ID fetched:", id);
 
-  console.log("Fetching Fixtures...");
-  const results = fetchArsenalFixtures(
-    api_key,
+  const requestURL = buildRequestURL(
     id,
+    leagueID,
+    today.getFullYear(),
     startOfThisWeek,
     endOfThisWeek,
   );
+  console.log("Fetching Fixtures from:", requestURL);
+  const results = await fetchArsenalFixtures(api_key, requestURL);
+  console.log("Fixtures fetched:", results.resultSet.count);
   const filePath = `${appRoot.path}/src/content/fixtures/${startOfThisWeek}-${endOfThisWeek}.json`;
   console.log("Writing data to file...", filePath);
   writeDataToFile(filePath, results);
