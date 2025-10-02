26fc803395b1c500172e22c9caae3bedd1dc3712
jtzero ? <jtzero511@gmail.com>
2025-10-01 22:46:58 -0400

    + lib/football-data module and test

---
+++ src/lib/football-data/competition.ts
@@ -0,0 +1,28 @@
+export const buildURL = (leagueCode: string) => {
+  return `https://api.football-data.org/v4/competitions/${leagueCode}`;
+};
+
+export const fetchPremierLeage = async (
+  api_key: string,
+  fetchFunction: Function = fetch,
+) => {
+  const headers = new Headers();
+  headers.append("X-Auth-Token", api_key);
+
+  const requestOptions: RequestInit = {
+    method: "GET",
+    headers: headers,
+    redirect: "follow",
+  };
+  const api_endpoint = `https://api.football-data.org/v4/competitions/PL`;
+  const response = await fetchFunction(api_endpoint, requestOptions);
+
+  const data = await response.json();
+  if (!response.ok) {
+    throw new Error(
+      `HTTP error! resonse status:${response.status}: ${data.Message}`,
+    );
+  }
+
+  return data;
+};
