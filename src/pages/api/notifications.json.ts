f4dc815815814dde8652a9257280ee2bba26eba0
jtzero ? <jtzero511@gmail.com>
2026-07-03 14:19:30 -0400

    improves notifs

--- src/pages/api/notifications.json.ts
+++ src/pages/api/notifications.json.ts
@@ -1,20 +1,28 @@
 import { getCollection, type CollectionEntry } from "astro:content";
 import type { APIRoute } from "astro";
 import type { NotificationAttributesType } from "@/content.types";
+import type { WithRequired } from "@/lib/withRequired";
+
+type PushNotification = WithRequired<NotificationAttributesType, "pushMessage">;
+
+interface PushNotificationCollectionEntry
+  extends CollectionEntry<"notifications"> {
+  data: PushNotification;
+}
 
 export const GET: APIRoute = async () => {
-  const notifications = await getCollection(
+  const notifications = (await getCollection(
     "notifications",
-    ({ data }) => data.send,
-  );
+    ({ data }) => data?.push && data?.pushMessage,
+  )) as PushNotificationCollectionEntry[];
+  console.log(notifications);
   return new Response(
     JSON.stringify(
       notifications.reduce(
         (
-          acc: { [key: string]: NotificationAttributesType },
-          curr: CollectionEntry<"notifications">,
+          acc: { [key: string]: PushNotification },
+          curr: PushNotificationCollectionEntry,
         ) => {
-          if (!curr.data.enabled) return acc;
           acc[curr.id] = curr.data;
           return acc;
         },
